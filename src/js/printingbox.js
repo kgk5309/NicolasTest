f210431767e62be024ecb39eafc2ba4be96b473d
kgk5309 <64968901+kgk5309@users.noreply.github.com>
2023-11-12 02:41:31 +0900

    1.0.0
    setState added

--- src/js/printingbox.js
+++ src/js/printingbox.js
@@ -1,3 +1,7 @@
+function setState(state) {
+    document.getElementById("state").innerText = state
+}
+
 function checkMobile() {
     var varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
     if (varUA.indexOf('android') > -1) {
@@ -18,6 +22,7 @@ function loginAction() {
     var loginData = document.getElementById("id-data").value
 
     if (checkMobile() == "android") {
+        setState("안드로이드 로그인")
         window.ReactNativeWebView.postMessage(JSON.stringify({
             type: "login",
             data: loginData
@@ -26,6 +31,7 @@ function loginAction() {
     }
 
     if (checkMobile() == 'ios') {
+        setState("아이폰 로그인")
         window.webkit.messageHandlers.login.postMessage(loginData)
     }
 
@@ -34,6 +40,7 @@ function loginAction() {
 // 네이티브 로그인 정보 초기화 기능
 function logoutAction() {
     if (checkMobile() == "android") {
+        setState("안드로이드 로그아웃")
         window.ReactNativeWebView.postMessage(
             JSON.stringify({
                 type: "logout"
@@ -42,6 +49,7 @@ function logoutAction() {
         return;
     }
     if (checkMobile() == "ios") {
+        setState("아이폰 로그아웃")
         window.webkit.messageHandlers.logout.postMessage()
     }
     document.getElementById("id-data").value = ""
@@ -51,6 +59,7 @@ function logoutAction() {
 // 저장된 로그인 정보 가져오기 기능
 function getLoginData() {
     if (checkMobile() == "android") {
+        setState("안드로이드 로그인 데이터 Get")
         window.ReactNativeWebView.postMessage(
             JSON.stringify({
                 type: "getLoginData"
@@ -59,6 +68,7 @@ function getLoginData() {
         return;
     }
     if (checkMobile() == "ios") {
+        setState("아이폰 로그인 데이터 Get")
         window.webkit.messageHandlers.getLoginData.postMessage()
     }
 }
